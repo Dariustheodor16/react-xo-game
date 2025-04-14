dafc5139d168c2df4cf624f7ddf2fe57a7876386
Dariustheodor16 <dariustheodor16@gmail.com>
2025-04-15 02:26:56 +0300

    Added page layout + new dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import GameBoardPage from "./pages/game-board.page";
+import GameMenuPage from "./pages/game-menu.page";
+import RegisterPage from "./pages/register.page";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>Hello World</StrictMode>
+  <StrictMode>
+    {/* Tip: It provides specific hooks and data globally accesible within child components */}
+    <BrowserRouter>
+      {/* Tip: The Routes must be a parent of any set of routes */}
+      <Routes>
+        {/* Tip: The route helps us to create a page */}
+        <Route index={true} element={<RegisterPage />} />
+        <Route path="game">
+          {/* We can access the page under "/game/menu" */}
+          <Route path="menu" element={<GameMenuPage />} />
+          <Route path="board" element={<GameBoardPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
