f206814ed5c1727e8e334a69f6a350cf896ff497
Dariustheodor16 <dariustheodor16@gmail.com>
2025-04-16 20:11:25 +0300

    Added game logic

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const initialState = {
   session: {
     playerName: "",
-    createdAt: "", // ISO 8601 - 2025-04-15T12:00:00Z
+    createdAt: "", // ISO 8601 - 2025-04-15T15:54:26.055Z
   },
   players: {
     player: {
-      mark: "", //"X" or "O"
-      timeout: -1, // -1 means no timeout
+      mark: "", // "o" or "x"
+      timeout: -1,
       wins: 0,
     },
     cpu: {
-      mark: "", //"X" or "O"
-      timeout: 10000, // 10 seconds
+      mark: "", // "o" or "x"
+      timeout: 10000, // 10s
       wins: 0,
     },
     ties: 0,
@@ -40,23 +40,92 @@ const initialState = {
       { winCombination: 7, mark: null },
       { winCombination: 8, mark: null },
     ],
-    playerTurn: "none", // "none", "o", "x"
+    playerTurn: "x", // "o" | "x"
+    playerWinner: null, // "o" | "x" | "tied"
+    metricsUpdated: false,
   },
 };
 
-const StoreContext = React.createContext(null);
+const StoreContext = React.createContext({
+  state: initialState,
+  dispatch: () => {},
+});
 
 const storeReducer = (data, action) => {
   switch (action.type) {
-    case "create": {
-      data.session = action.payload;
-      return data;
+    case "set-players-mark": {
+      return {
+        ...data,
+        players: {
+          ...data.players,
+          player: {
+            ...data.players.player,
+            mark: action.payload.player, // "o" or "x"
+          },
+          cpu: {
+            ...data.players.cpu,
+            mark: action.payload.cpu, // "o" or "x"
+          },
+        },
+        game: {
+          ...data.game,
+          playerTurn: "x",
+        },
+      };
+    }
+    case "set-game-board-grid-list": {
+      return {
+        ...data,
+        game: {
+          ...data.game,
+          boardGridList: action.payload,
+        },
+      };
+    }
+    case "set-game-player-turn": {
+      return {
+        ...data,
+        game: {
+          ...data.game,
+          playerTurn: action.payload,
+        },
+      };
+    }
+    case "set-game-player-winner": {
+      return {
+        ...data,
+        game: {
+          ...data.game,
+          playerWinner: action.payload,
+        },
+      };
+    }
+    case "set-players-metrics": {
+      return {
+        ...data,
+        players: {
+          ...data.players,
+          player: {
+            ...data.players.player,
+            wins: data.players.player.wins + action.payload.playerWins,
+          },
+          cpu: {
+            ...data.players.cpu,
+            wins: data.players.cpu.wins + action.payload.cpuWins,
+          },
+          ties: data.players.ties + action.payload.ties,
+        },
+        game: {
+          ...data.game,
+          metricsUpdated: true,
+        },
+      };
     }
   }
 };
 
-const StoreProvider = () => {
-  // useReducer helps us to create a more complex state over useState
+const StoreProvider = (props) => {
+  // Tip: useReducer helps us to create a more complex state like useState does
   const [state, dispatch] = React.useReducer(storeReducer, initialState);
   return (
     <StoreContext.Provider value={{ state, dispatch }}>
@@ -65,7 +134,7 @@ const StoreProvider = () => {
   );
 };
 
-// Tip: Custom hook to retrieve the context value within any child component
+// Tip: Custom hook to retrieve the context value within any child components
 export const useStore = () => {
   return React.useContext(StoreContext);
 };
